6b08884e0e3f05ca1125f628eb005af81d41a30e
Ihtisham Saeed <111792380+ihtishamsaeed414@users.noreply.github.com>
2024-07-03 10:18:06 +0500

    inital commit

---
+++ src/StartScreen.js
@@ -0,0 +1,20 @@
+import { type } from "@testing-library/user-event/dist/type";
+
+function StartScreen({ numQuestions, dispatch }) {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz</h2>
+      <h3>
+        {numQuestions} questions to test your React master
+        <button
+          className="btn btn-ui"
+          onClick={() => dispatch({ type: "start" })}
+        >
+          Let's start
+        </button>
+      </h3>
+    </div>
+  );
+}
+
+export default StartScreen;
