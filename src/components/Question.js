9a966c49130fa2dcb04506d75105bcaedd620978
Ihtisham Saeed <111792380+ihtishamsaeed414@users.noreply.github.com>
2024-07-04 10:47:01 +0500

    selecting the option Functionality

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,10 @@
-function Question({ question }) {
+import Options from "./Options";
+function Question({ question, dispatch, answer }) {
+  // console.log(question);
   return (
     <div>
       <h4>{question.question}</h4>
-      <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>
-            {option}
-          </button>
-        ))}
-      </div>
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
